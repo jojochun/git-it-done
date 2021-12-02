0a666adb58aab41cbdb97f35d849fc7a08f405e4
jochun <jkchun@protonmail.com>
2021-12-01 15:03:45 -1000

    add function to display warning for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 // function to take in repo name as a parameter
@@ -13,6 +13,11 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -57,10 +62,21 @@ var displayIssues = function (issues) {
         issueEl.appendChild(typeEl);
 
         issueContainerEl.appendChild(issueEl);
-
-
     }
-
 };
 
+// function to displayWarning, over 30 issues
+var displayWarning = function (repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+};
 
